345ca4023acb298d2d2119396cdf719383ec2b02
thanaponz <fook.thanapon@gmail.com>
2017-10-21 16:27:48 +0700

    2

---
+++ src/routing-config.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Redirect } from 'react-router'
+import { Home, BasicRouting, Blocking, Miss, QueryParams, Recursive, Login, ProtectedPage} from './Components'
+import fakeAuth from './Auth'
+
+const routes = [
+  {
+    'path':'/',
+    'component': Home,
+    'exact': true
+  },
+  {
+    'path':'/BasicRouting',
+    'component': BasicRouting
+  },
+  {
+    'path':'/Blocking',
+    'component': Blocking
+  },
+  {
+    'path':'/Miss',
+    'component': Miss
+  },
+  {
+    'path':'/QueryParams',
+    'component': QueryParams
+  },
+  {
+    'path':'/Recursive',
+    'component': Recursive
+  },
+  {
+    'path':'/login',
+    'component': Login
+  },
+  {
+    'path':'/Protected',
+    'component': () => (fakeAuth.isAuthenticated ? (<ProtectedPage />) : (<Redirect to={{pathname: '/login',state: { from: '/Protected'}
+    }}/>))
+  }
+  
+];
+
+export default routes;
